66d4ba78472870c13018707aa2498440456e481b
ingvord <mail@ingvord.ru>
2018-05-15 13:03:22 +0200

    Progress: enable logging

--- src/main/java/org/tango/webapp/UserContextCache.java
+++ src/main/java/org/tango/webapp/UserContextCache.java
@@ -3,6 +3,8 @@ package org.tango.webapp;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -17,6 +19,8 @@ import java.util.concurrent.TimeUnit;
  * @since 5/14/18
  */
 public class UserContextCache implements UserContextStorage {
+    private final Logger logger = LoggerFactory.getLogger(UserContextCache.class);
+    
     private final UserContextStorage delegate;
     private final ExecutorService exec = Executors.newCachedThreadPool();
 
@@ -38,37 +42,37 @@ public class UserContextCache implements UserContextStorage {
     }
 
     @Override
-    public void save(String userName, String data) throws IOException {
+    public void save(String userName, String data) {
         cache.put(userName, data);
         exec.submit(() -> {
             try {
                 delegate.save(userName, data);
             } catch (IOException e) {
-                //TODO logger
+                logger.error("Failed to save data for user[{}] due to {}", userName, e.getMessage());
                 cache.invalidate(userName);
             }
         });
     }
 
     @Override
-    public void delete(final String userName) throws IOException {
+    public void delete(final String userName) {
         cache.invalidate(userName);
 
         exec.submit(() -> {
             try {
                 delegate.delete(userName);
             } catch (IOException e) {
-                //TODO logger
+                logger.error("Failed to delete data for user[{}] due to {}", userName, e.getMessage());
             }
         });
     }
 
     @Override
-    public String load(String userName) throws IOException {
+    public String load(String userName) {
         try {
             return cache.get(userName);
         } catch (ExecutionException e) {
-            //TODO logger
+            logger.error("Failed to get data for user[{}]. Returning null...", userName);
             return null;
         }
     }
